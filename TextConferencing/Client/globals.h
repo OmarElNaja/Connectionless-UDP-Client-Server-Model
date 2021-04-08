ba51450133b7f43776d90f9252a172a37b09a0fc
OmarElNaja <57801361+OmarElNaja@users.noreply.github.com>
2021-04-08 09:23:59 -0300

    Add files via upload

---
+++ TextConferencing/Client/globals.h
@@ -0,0 +1,13 @@
+#ifndef __GLOBALS__
+#define __GLOBALS__
+
+#define PACKETLEN 512
+#define MAX_DATA 255
+#define MAX_NAME 25
+#define MAX_INPUT 1024 // max number of bytes we can get at for input
+#define MAX_NUM_USERS 48
+#define MAX_NUM_SESSIONS 48 // equal to max number of users
+
+#define STDIN 0
+
+#endif
